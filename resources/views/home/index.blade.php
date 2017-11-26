44a76ddac7b92c2d0e4e18ace74b3982198130d9
Ville Viklund <ville.viklund@morgan.fi>
2017-11-26 17:39:51 +0200

    Style and text improvements

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layout')
 
 @section('content')
-    <p>Tänään päivystää: </p>
+    <p>Tällä viikolla Vaasan päivystävä apteekki on:</p>
     <h1>{{ $storeName }}</h1>
+
+    <p class="external-links">
+        <a href="https://www.vaasa.fi/paivystavat-apteekit">Apteekkien päivystys- ja yhteystiedot</a>
+    </p>
 @endsection
