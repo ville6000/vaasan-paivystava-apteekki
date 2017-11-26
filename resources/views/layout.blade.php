5dc2b57ac2cbd6e37ff66e5d24ca0558e8dfb87f
Ville Viklund <ville.viklund@morgan.fi>
2017-11-26 18:30:03 +0200

    Add Analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,5 +17,10 @@
     @yield('content')
 </main>
 
+<script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create', '<?php echo env('ANALYTICS'); ?>','auto');ga('send','pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
 </body>
 </html>
