a666f5324f76eebbcb83b5e18991b3cb5dda7e76
Ville Viklund <ville.viklund@morgan.fi>
2017-11-27 10:15:11 +0200

    Add meta description

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Vaasan päivystävä apteekki</title>
-    <meta name="description" content="">
+    <meta name="description" content="Vaasan päivystävä apteekki">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700|Ubuntu:700" rel="stylesheet">
