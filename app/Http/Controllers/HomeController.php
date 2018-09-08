ee49411ec65dea203a383c92fd1aebcb5e9e2fbd
Ville Viklund <ville.viklund@morgan.fi>
2018-09-08 13:23:04 +0300

    Use cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Drugstore\Drugstore;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $drugStore = new Drugstore();
-        $storeName = $drugStore->getOnCallDrugstore();
+        $storeName = Cache::get('store');
+
+        if (!$storeName) {
+            $storeName = $drugStore->getOnCallDrugstore();
+	        Cache::put('store', $storeName, 60);
+        }
 
         $monday = date('d.m', strtotime('monday this week'));
         $sunday = date('d.m', strtotime('sunday this week'));
